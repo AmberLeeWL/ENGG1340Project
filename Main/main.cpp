3371d5636af298e396a0e060e166ed0517872b35
Amber <47201222+AmberLeeWL@users.noreply.github.com>
2019-04-09 11:48:09 +0800

    Update main.cpp

--- Main/main.cpp
+++ Main/main.cpp
@@ -4,5 +4,12 @@
 using namespace std;
 
 main{
-
+  string user[30];
+  string password[20];
+  cout << "-Welcome to Accounting System v0.1-" << endl;
+  cout << " " << endl;
+  cout << "Please enter your name: ";
+  cin >> user;
+  cout << "Please enter your password: ";
+  cin >> password;
 }
