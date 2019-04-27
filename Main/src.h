e2b20f1ecce3c07161f22c94b5a458878aa6ba8c
Amber <47201222+AmberLeeWL@users.noreply.github.com>
2019-04-27 21:22:58 +0800

    Create src.h

---
+++ Main/src.h
@@ -0,0 +1,44 @@
+//src.h
+
+#ifndef scr_h
+#define scr_h
+
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <list>
+#include <ctime>
+#include <fstream>
+#include<stdio.h>
+#include<stdlib.h>
+
+using namespace std;
+
+const int MAX_data = 1000;
+const char logfilename[80] = "SystemLog.txt";
+const char userfilename[80] = "User.txt";
+const char userinfoname[80] = "UserInfo.txt";
+const int stdwidth = 69;
+
+struct userinfo{
+    char Name[40];
+    char Password[30];
+    char Occupation[30];
+    double Income;
+    double Expense[5];
+    double Budget;
+    double Account[3];
+};
+
+void printbreak(){
+    for (int i=0;i<stdwidth;i++){
+        if (i==stdwidth-1){
+            cout << '-'<<endl;
+        }
+        else {
+            cout << '-';
+        }
+    }
+}
+
+#endif
