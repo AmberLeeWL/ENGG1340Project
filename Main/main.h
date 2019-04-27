32c73a10e7c4a30ecdec222dae077e474067e559
Amber <47201222+AmberLeeWL@users.noreply.github.com>
2019-04-27 21:35:51 +0800

    Create main.h

---
+++ Main/main.h
@@ -0,0 +1,23 @@
+//main.h
+
+#ifndef Header_h
+#define Header_h
+
+#include "src.h"
+#include "Add.cpp"
+#include "Print.cpp"
+
+using namespace std;
+
+struct userlogin{
+    string n;
+    string p;
+};
+
+void printoption();
+
+string quit();
+
+int main();
+
+#endif
