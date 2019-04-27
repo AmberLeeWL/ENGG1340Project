c1ca1bb6577da756d28021ca2b244584e52100b7
WinnieTsui <43720210+WinnieTsui@users.noreply.github.com>
2019-04-27 10:37:18 +0800

    Update Delete.cpp

--- Main/Delete.cpp
+++ Main/Delete.cpp
@@ -1,8 +1,56 @@
 //Delete record function
 
 #include <iostream>
+#include <iomanip>
+#include <string>
+#include <fstream>
+
 using namespace std;
 
-main{
+const int add_stdwidth = 69;
 
+void printbreak() {
+  for (int i=0;i<add_stdwidth;i++){
+      if (i==add_stdwidth-1) {
+        cout << '-' <<endl;
+      }
+      else {
+           cout << '-';
+      }
+  }
+}
+
+int Delete(string userfilename,string userinfoname){
+  ifstream fin;
+  
+  cout << setw(42);
+  cout << "-Delete Record-" << endl;
+  fin.open(userfilename);
+  if ( fin.fail() ){
+    cout << userfilename << " does not exist" << endl;
+    exit(1);
+  }
+  fin.open(userinfofilename);
+  if ( fin.fail() ){
+    cout <<  userinfofilename << " does not exist" << endl;
+    exit(1);
+  }
+  else{
+    printbreak();
+      char choice;
+      cout << "Please enter the username going to be deleted: ";
+      cin >> username;
+      
+      cout << "Confirm (Y/N): " ;
+      cin >> choice;
+      if (choice == 'Y' || choice == 'y'){
+         remove(userfilename);
+         remove(userinfofilename);
+         cout << "User <username> deleted! \n";
+      }
+      else{
+        cout << "User <username> has not been deleted! \n";
+      }
+  }
+  return 0;
 }
