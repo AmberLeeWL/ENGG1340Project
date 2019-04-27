7e0ce7374027fbc1ed8cd1e9f39ae8a7f305cd40
WinnieTsui <43720210+WinnieTsui@users.noreply.github.com>
2019-04-27 22:22:03 +0800

    Create Edit.h

---
+++ Main/Edit.h
@@ -0,0 +1,16 @@
+//Edit.h
+
+#ifndef Header_h
+#define Header_h
+
+#include "src.h"
+
+using namespace std;
+
+void printbreak();
+
+void printoption();
+
+int Edit(string userinfofilenmae, string userfilename);
+
+#endif
