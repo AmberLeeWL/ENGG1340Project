78e5338e2dbf47dee78c926a1b9d84c3556ee4f5
WinnieTsui <43720210+WinnieTsui@users.noreply.github.com>
2019-04-27 12:31:23 +0800

    Create Delete.h

---
+++ Main/Delete.h
@@ -0,0 +1,12 @@
+//Delete.h
+
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <fstream>
+
+const int add_stdwidth = 69;
+
+void printbreak();
+
+int Delete(string userfilename,string userinfoname);
