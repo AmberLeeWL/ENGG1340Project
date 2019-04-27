177be3496f93197d2121c626cf48f91fb3e994f9
Amber <47201222+AmberLeeWL@users.noreply.github.com>
2019-04-27 21:19:45 +0800

    Update main.cpp
    Delete.cpp and Edit.cpp not being linked

--- Main/main.cpp
+++ Main/main.cpp
@@ -1,28 +1,15 @@
-//Main
+//main.cpp
 
-#include <iostream>
-#include <iomanip>
-#include <string>
-#include <list>
-#include <ctime>
-#include <fstream>
+#include "src.h"
+#include "add.cpp"
+#include "print.h"
 
 using namespace std;
 
-const int stdwidth = 69;
-const char logfilename[80] = "SystemLog.txt";
-const char userfilename[80] = "User.txt";
-
-void printbreak(){
-    for (int i=0;i<stdwidth;i++){
-        if (i==stdwidth-1){
-            cout << '-'<<endl;
-        }
-        else {
-            cout << '-';
-        }
-    }
-}
+struct userlogin{
+    string n;
+    string p;
+};
 
 void printoption(){
     cout << "Functions available: " << endl;
@@ -51,34 +38,41 @@ string quit(){
 }
 
 int main(){
-    char user[30];
+    char username[30];
     char password[20];
     int option=0;
     string temp,in_date,out_date;
     ifstream fin;
     ifstream fuser;
+    ifstream finfo;
     ofstream fout;
+    string name[MAX_data];
+    string pw[MAX_data];
+    int count = 0;
+    userinfo user[MAX_data];
+    int u;
     
     cout << setw(52);
     cout << "-Welcome to Accounting System v0.1-" << endl;
     fin.open(logfilename);
     fuser.open(userfilename);
+    finfo.open(userinfoname);
     fout.open(logfilename,ios::app);
     if ( fin.fail() ) {
         cout << "Error in opening file " << logfilename << endl;
         exit(1);
     }
     else{
-        fin.seekg(-1, std::ios_base::end);
+        fin.seekg(-1,ios_base::end);
         if(fin.peek() == '\n'){
-            fin.seekg(-1, std::ios_base::cur);
+            fin.seekg(-1,ios_base::cur);
             int i = fin.tellg();
-            for(i;i>0; i--){
+            for(i;i>0;i--){
                 if(fin.peek() == '\n'){
                     fin.get();
                     break;
                 }
-                fin.seekg(i, std::ios_base::beg);
+                fin.seekg(i,ios_base::beg);
             }
         }
         getline(fin,in_date);
@@ -86,6 +80,20 @@ int main(){
     if ( fuser.fail() ) {
         cout << "Error in opening file " << userfilename << endl;
         exit(1);
+    }
+    if ( finfo.fail() ) {
+        cout << "Error in opening file " << userinfoname << endl;
+        exit(1);
+    }
+    int t = 0;
+    string line;
+    while (getline(fuser,line)){
+        t++;
+    }
+    count = t/2;
+    string* userpw = new string[t];
+    for (int i = 0;i<t;i++){
+        getline(fuser,userpw[i]);
     }
     if ( fout.fail() ) {
         cout << "Error in opening file " << logfilename << endl;
@@ -96,12 +104,51 @@ int main(){
         cout << "Last modified on: " << in_date << endl;
     }
     printbreak();
+    for (int k=0;k<count;k++){
+        finfo >> user[k].Name;
+        finfo >> user[k].Password;
+        finfo >> user[k].Occupation;
+        finfo >> user[k].Income;
+        finfo >> user[k].Expense[0];
+        finfo >> user[k].Expense[1];
+        finfo >> user[k].Expense[2];
+        finfo >> user[k].Expense[3];
+        finfo >> user[k].Expense[4];
+        finfo >> user[k].Budget;
+        finfo >> user[k].Account[0];
+        finfo >> user[k].Account[1];
+        finfo >> user[k].Account[2];
+    }
+    
     cout << "Please enter your name    : ";
-    cin >> user;
-    cout << "Please enter your password: ";
-    cin >> password;
+    cin >> username;
+    for (int j =0;j<t;j++){
+        if ((username != userpw[j]) && j==t-1){
+            char addnew;
+            cout << "No matching user, add new user?(Y/N): ";
+            cin >>addnew;
+            if (addnew == 'Y' || addnew == 'y'){
+                add_main(userinfoname, userfilename);
+            }
+            if (addnew == 'N' || addnew == 'n'){
+                exit(1);
+            }
+        }
+        if (username == userpw[j]){
+            cout << "Please enter your password: ";
+            cin >> password;
+            if (password == userpw[j+1]){
+                for (int n=0;n<count;n++){
+                    if (user[n].Name == username){
+                        u = n;
+                        break;
+                    }
+                }
+            }
+        }
+    }
     printbreak();
-    cout << "Hello, " << user << "!"<<endl;
+    cout << "Hello, " << username << "!"<<endl;
     cout << " " << endl;
     printoption();
     cout << "Please choose the functions(1-5): ";
@@ -112,16 +159,15 @@ int main(){
         cin >> option;
     }
     switch(option){
-        case 1: cout << "Heading to \"1. Add record\"" << "..." << endl; //to be replaced by function in Add.cpp
-        case 2: cout << "Heading to \"2. Delete record\"" << "..." << endl; //to be replaced by function in Delete.cpp
-        case 3: cout << "Heading to \"3. Edit record\"" << "..." << endl; //to be replaced by function in Edit.cpp
-        case 4: cout << "Heading to \"4. Print report\"" << "..." << endl; //to be replaced by function in Print.cpp
+        case 1: add_main(userinfoname,userfilename);
+        case 2: cout << "Heading to \"2. Delete record\"" << "..." << endl;// To be replaced by Delete.cpp
+        case 3: cout << "Heading to \"3. Edit record\"" << "..." << endl; // To be replaced by Edit.cpp
+        case 4: print_main(user[u]);
         case 5: out_date = quit();
     }
-    fout << out_date << " (" << user << ")" << endl;
+    fout << out_date << " (" << username << ")" << endl;
     fin.close();
     fout.close();
     cout << " " << endl;
-    cout << "Saved date: " << out_date << endl;
     return 0;
 }
